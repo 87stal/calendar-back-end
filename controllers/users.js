d3ad6ce80d6ea6f6b927f627672dd78555f8d674
87stal <87stal@gmail.com>
2021-04-25 18:16:38 +0300

    Edit register

--- controllers/users.js
+++ controllers/users.js
@@ -23,12 +23,10 @@ const reg = async (req, res, next) => {
         message: 'Email in use'
       })
     }
-    const verificationToken = nanoid()
-   
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '2h' })
     const newUser = await Users.create({
-      ...req.body,
-      verify: false,
-      verificationToken
+      ...req.body
     })
     return res.status(HttpCode.CREATED).json({
       status: 'success',
@@ -36,8 +34,7 @@ const reg = async (req, res, next) => {
       ResponseBody: {
         user: {
           email: newUser.email,
-          subscription: newUser.subscription,
-          avatar: newUser.avatar
+          token: token
         }
       }
     })
@@ -50,7 +47,7 @@ const login = async (req, res, next) => {
   try {
     const { email, password } = req.body
     const user = await Users.findByEmail(email)
-    if (!user || !user.validPassword(password) || !user.verify) {
+    if (!user || !user.validPassword(password)) {
       return res.status(HttpCode.UNAUTHORIZED).json({
         status: 'error',
         code: HttpCode.UNAUTHORIZED,
@@ -96,62 +93,7 @@ const currentUser = (req, res, next) => {
   })
 }
 
-const avatars = async (req, res, next) => {
-  try {
-    const id = req.user.id
-    const avatar = await saveAvatarToStatic(req)
-    await Users.updateAvatar(id, avatar)
-    return res.json({
-      status: 'success',
-      code: HttpCode.OK,
-      data: {
-        avatar
-      }
-    })
-  } catch (e) {
-    next(e)
-  }
-}
 
-const saveAvatarToStatic = async (req) => {
-  const id = req.user.id
-  const AVATARS_OF_USERS = process.env.AVATARS_OF_USERS
-  const pathFile = req.file.path
-  const newNameAvatar = `${Date.now()}-${req.file.originalname}`
-  const img = await Jimp.read(pathFile)
-  await img
-    .autocrop()
-    .cover(250, 250, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
-    .writeAsync(pathFile)
-  await createFolderIsExist(path.join(AVATARS_OF_USERS, id))
-  await fs.rename(pathFile, path.join(AVATARS_OF_USERS, id, newNameAvatar))
-  const avatarUrl = path.normalize(path.join(id, newNameAvatar))
-  try {
-    await fs.unlink(path.join(process.cwd(), AVATARS_OF_USERS, req.user.avatar))
-  } catch (e) {
-    console.log(e.message)
-  }
-  return avatarUrl
-}
-const verify = async (req, res, next) => {
-  try {
-    const user = await Users.findByVerifyToken(req.params.verificationToken)
-    const verify = req.params.verify
-    if (user) {
-      await Users.updateVerifyToken(user.id, true, null)
-      return res.json({
-        status: 'success',
-        code: HttpCode.OK,
-        message: 'Verification successful!'
-      })
-    }
-    return res.status(HttpCode.NOT_FOUND).json({
-      status: 'error',
-      code: HttpCode.NOT_FOUND,
-      message: 'Email already verify'
-    })
-  } catch (e) {
-  
-  }
-}
-module.exports = { reg, login, logout, currentUser, avatars, verify }
+
+
+module.exports = { reg, login, logout, currentUser }
